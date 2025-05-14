d981666a9db507a7d4f969d2b7a87e6bfd68fe00
abeljim <abelj1@uci.edu>
2025-05-13 20:32:02 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Nix Flake for Silabs Commander Cli
---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "Silicon Labs Commander CLI";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {system = "x86_64-linux";};
+  in {
+    packages.${system}.commander-cli = pkgs.stdenv.mkDerivation {
+      pname = "commander-cli";
+      version = "1.18.1";
+
+      src = pkgs.fetchurl {
+        url = "https://github.com/abeljim/commander-cli-nix/releases/download/v1.18.1/commander_cli_linux_1.18.1.zip";
+        sha256 = "12w0c5l0nkm90v3j0xir04wrm32yyf5j33b89n9z84qawqbbacdm";
+      };
+
+      buildInputs = [
+        pkgs.libarchive
+        # pkgs.jdk17
+      ];
+
+      unpackPhase = ''
+        bsdtar -xf $src
+      '';
+
+      installPhase = ''
+        mkdir -p $out/bin
+        mv commander-cli $out/
+        chmod +x $out/commander-cli/commander-cli
+
+        ln -s $out/commander-cli/commander-cli $out/bin/commander-cli
+      '';
+
+      meta = with pkgs.lib; {
+        description = "Silicon Labs Commander CLI";
+        homepage = "https://www.silabs.com/developer-tools/simplicity-studio/simplicity-commander";
+        license = licenses.unfree;
+        platforms = platforms.linux;
+        maintainers = with maintainers; [abeljim];
+      };
+    };
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        self.packages.${system}.commander-cli
+      ];
+    };
+  };
+}
